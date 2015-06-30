0e9effded3b23ceb680fc87839c70ebcd95d1f8a
rsandu <razvi.sandu@gmail.com>
2015-06-30 16:04:55 -0600

    Clicking on Blue Square plays "JOHN CENA!" audio file.
    Also added overlay for image (really basic lightbox; click on blue
    square, everything else is shaded and image of john cena pops up.)

--- script/basicScript.js
+++ script/basicScript.js
@@ -3,7 +3,38 @@
  */
 var countEr = 0;
 
+var $overlay = $('<div id = "overlay"></div>');
+var $image = $('<img src="http://www.wwe.com/f/styles/superstar_bio/public/talent/bio/2015/01/johncena_bio_20150119.png"/>');
+
+///// SETTING THE JOHN CENA AUDIO FILE
+$(document).ready(function(){
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'http://puu.sh/iIJqP/7ca580ba60.wav')
+    // Load it
+    $.get();
+    audioElement.addEventListener("load", function() {
+        audioElement.play();
+    }, true);
+
+    $("#johncena").click(function(){
+        audioElement.play();
+    })
+})
+var jcena = {};
+jcena["yell"] = new Audio();
+jcena["yell"].src = ""
+$("body").append($overlay);
+
 $("#welcome").click(function(){
     countEr += 1;
     $("#clicker").text(countEr);
+})
+
+$("#johncena").click(function(){
+    $overlay.append($image);
+    $overlay.show();
+})
+
+$($overlay).click(function(){
+    $overlay.hide();
 })
