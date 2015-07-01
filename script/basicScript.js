87ce34d4ca02f1c498ce3e640c95f35c2c8d9cf7
rsandu <razvi.sandu@gmail.com>
2015-06-30 18:13:48 -0600

    Inserted Red Square that moves with Arrow Keys

--- script/basicScript.js
+++ script/basicScript.js
@@ -5,8 +5,10 @@ var countEr = 0;
 
 var $overlay = $('<div id = "overlay"></div>');
 var $image = $('<img src="http://www.wwe.com/f/styles/superstar_bio/public/talent/bio/2015/01/johncena_bio_20150119.png"/>');
+var $mover = $('#mover');
 
 ///// SETTING THE JOHN CENA AUDIO FILE
+
 $(document).ready(function(){
     var audioElement = document.createElement('audio');
     audioElement.setAttribute('src', 'http://puu.sh/iIJqP/7ca580ba60.wav')
@@ -22,22 +24,42 @@ $(document).ready(function(){
 })
 var jcena = {};
 jcena["yell"] = new Audio();
-jcena["yell"].src = ""
+jcena["yell"].src = "";
 
-//// END OF JOHN CENA AUDIO FILE SETUP
+//// END OF JOHN CENA AUDIO FILE SETUP START OF OVERLAY (JOHN CENA IMAGE SHOWER + AUDIO PLAYER SETUP)
 
 $("body").append($overlay);
 
 $("#welcome").click(function(){
     countEr += 1;
     $("#clicker").text(countEr);
-})
+});
 
 $("#johncena").click(function(){
     $overlay.append($image);
     $overlay.show();
-})
+});
 
 $($overlay).click(function(){
     $overlay.hide();
-})
+});
+
+//// END OF OVERLAY AND START OF MOVE BLOCK
+
+$(document).keydown(function(e){
+    switch(e.which){
+        case 37:
+            $mover.css('left', $mover.offset().left - 10);
+            break;
+        case 38:
+            $mover.css('top', $mover.offset().top - 10);
+            break;
+        case 39:
+            $mover.css('left', $mover.offset().left + 10);
+            e.preventDefault();
+            break;
+        case 40:
+            $mover.css('top', $mover.offset().top + 10);
+            break;
+    }
+});
